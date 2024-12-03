5415434c517178737d3731f1b92e63b641c9852a
xingyu <xingyu@hylink.net.cn>
2024-12-03 16:41:15 +0800

    修改代码

--- src/main/java/cn/net/explorer/controller/UserController.java
+++ src/main/java/cn/net/explorer/controller/UserController.java
@@ -1,26 +1,42 @@
 package cn.net.explorer.controller;
 
 import cn.dev33.satoken.stp.StpUtil;
+import cn.net.explorer.ExplorerApplication;
 import cn.net.explorer.domain.response.ApiResponse;
 import cn.net.explorer.service.UserService;
 import com.alibaba.fastjson.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
 
     @Autowired
     private UserService userService;
 
-    @Value("classpath:/static/data/nav.json")
-    private Resource resource;
+    @Autowired
+    private ResourceLoader resourceLoader;
+
+    @GetMapping("/print")
+    public ApiResponse<?> print(){
+        String username = System.getenv("ENV_USERNAME");
+        logger.info("username : {}",username);
+        return ApiResponse.ok(username);
+    }
 
     @PostMapping("/login")
     public ApiResponse<String> login(@RequestBody JSONObject parameter) {
@@ -34,8 +50,19 @@ public class UserController {
 
     @GetMapping("/menus")
     public JSONObject menus() throws Exception {
-        String str = new String(Files.readAllBytes(Paths.get(resource.getURI())));
-        return JSONObject.parseObject(str);
+        // 获取资源
+        Resource resource = resourceLoader.getResource("classpath:/static/data/nav.json");
+
+        // 读取文件内容并转换为字符串
+        try (InputStream inputStream = resource.getInputStream(); BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
+            StringBuilder stringBuilder = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                stringBuilder.append(line).append(System.lineSeparator());
+            }
+            return JSONObject.parseObject(stringBuilder.toString());
+        }
+
     }
 
     @PostMapping("/loginOut")
--- src/main/java/cn/net/explorer/service/impl/UserServiceImpl.java
+++ src/main/java/cn/net/explorer/service/impl/UserServiceImpl.java
@@ -6,6 +6,7 @@ import cn.net.explorer.domain.response.ApiResponse;
 import cn.net.explorer.exception.BusinessException;
 import cn.net.explorer.service.UserService;
 import com.alibaba.fastjson.JSONObject;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,17 +16,11 @@ import java.util.Objects;
 @Service
 public class UserServiceImpl implements UserService {
 
-    @Value("${sa-token.default-username}")
-    private String defaultUserName;
-    @Value("${sa-token.default-password}")
-    private String defaultPassword;
 
     @Override
     public ApiResponse<String> login(  JSONObject parameter) {
-        String username = parameter.getString("username");
-        String password = parameter.getString("password");
-        if (!Objects.equals(username, defaultUserName)) throw new BusinessException("用户名不存在");
-        if (!Objects.equals(password, defaultPassword)) throw new BusinessException("密码错误");
+        if (!Objects.equals(parameter.getString("username"), System.getenv("ENV_USERNAME"))) throw new BusinessException("用户名不存在");
+        if (!Objects.equals(parameter.getString("password"), System.getenv("ENV_PASSWORD"))) throw new BusinessException("密码错误");
 
         StpUtil.login(1);
         return ApiResponse.ok(StpUtil.getTokenValue());
@@ -35,7 +30,7 @@ public class UserServiceImpl implements UserService {
     public ApiResponse<JSONObject> info() {
         JSONObject jb = new JSONObject();
         jb.put("id", 1);
-        jb.put("username", defaultUserName);
+        jb.put("username",  System.getenv("ENV_USERNAME"));
         return ApiResponse.ok(jb);
     }
 }
